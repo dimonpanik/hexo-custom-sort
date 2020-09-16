55fc09cee595d45980d673d9a829380139733275
dimonpanik <admin@destonik.ru>
2020-09-16 16:03:08 +0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# hexo-custom-sort# temp
 # hexo-custom-sort
+Plugin helps to sort posts by required fields 
+## Usage
+
+**in _config.yml**
+```
+custom_sort:
+  params: [one,two]
+  pathPage: 'optional'
+  perPage: 'optional'
+
+```
+All selected parameters will be moved to locals
+
+param.name = Name no slugize
+param.path = slugize path
+
+Example:
+_config.yml
+```
+custom_sort:
+  params: [collection]
+
+```
+_particle/menu.js
+```
+<% site.collection.forEach(coll=>{ %>
+    <a href="/<%= coll.path %>"><%= coll.name %></a> 
+<%})%>
+
+```
--- index.js
+++ index.js
@@ -3,77 +3,98 @@ const util = require('hexo-util');
 const slugize = util.slugize
 
 hexo.config.custom_sort = Object.assign({
-    params: hexo.config.custom_sort.params,
+    fields: hexo.config.custom_sort.fields,
     pathPage: hexo.config.custom_sort.pathPage || 'page',
     perPage: hexo.config.custom_sort.perPage || 20
 }, hexo.config.custom_sort);
 
-hexo.extend.generator.register('hexo-custom-sort', function(locals){
+
+
+const last = {}
+
+hexo.extend.generator.register('hexo-custom-sort', function (locals) {
 
     const allPosts = locals.posts;
-    const parameters = []
-    hexo.config.custom_sort.params.forEach(item=>{
-        var tmp = item.split('|')
-        if(!Object.prototype.hasOwnProperty.call(parameters,item)){
-            parameters[tmp[0]] = tmp[1]
-        }
-    })
+    const parameters = hexo.config.custom_sort.fields
     const ItemPosts = {}
-    const paramsKeys = Object.keys(parameters)
     let result = []
 
     function generate(path, posts, options) {
         options = options || {};
-    
-        result = result.concat(pagination(path, posts, {
-          perPage: hexo.config.custom_sort.perPage,
-          layout: ['tag', 'index'],
-          format: hexo.config.custom_sort.pathPage + '/%d/',
-          data: options
-        }));
-      }
 
-        allPosts.forEach(post =>{
-            for(const[key,value] of Object.entries(parameters)){
-                if(typeof(post[key]) !== 'undefined'){  
-                    if(typeof(post[key]) !== 'object'){
-                        if(!Object.prototype.hasOwnProperty.call(ItemPosts, key)){
+        result = result.concat(pagination(path, posts, {
+            perPage: hexo.config.custom_sort.perPage,
+            layout: ['tag', 'index'],
+            format: hexo.config.custom_sort.pathPage + '/%d/',
+            data: options
+        }));
+    }
+
+
+
+    allPosts.forEach(post => {
+        parameters.forEach(key => {
+            if (typeof (post[key]) !== 'undefined') {
+                if (typeof (post[key]) !== 'object') {
+                    if (!Object.prototype.hasOwnProperty.call(ItemPosts, key)) {
+                        ItemPosts[key] = []
+                    }
+                    if (!Object.prototype.hasOwnProperty.call(ItemPosts[key], post[key])) {
+                        ItemPosts[key][post[key]] = [];
+                    }
+                    ItemPosts[key][post[key]].push(post)
+                } else {
+                    post[key].forEach(coll => {
+                        if (!Object.prototype.hasOwnProperty.call(ItemPosts, key)) {
                             ItemPosts[key] = []
                         }
-                        if (!Object.prototype.hasOwnProperty.call(ItemPosts[key], post[key])) {
-                            ItemPosts[key][post[key]] = [];
+                        if (!Object.prototype.hasOwnProperty.call(ItemPosts[key], coll)) {
+                            ItemPosts[key][coll] = [];
                         }
-                        ItemPosts[key][post[key]].push(post)
-                    }else{
-                        post[key].forEach(coll =>{
-                            if(!Object.prototype.hasOwnProperty.call(ItemPosts,key)){
-                                ItemPosts[key] = []
-                            }
-                            if (!Object.prototype.hasOwnProperty.call(ItemPosts[key], coll)) {
-                                ItemPosts[key][coll] = [];
-                            }
-                            ItemPosts[key][coll].push(post)
-                        })
-                    }
-                    
+                        ItemPosts[key][coll].push(post)
+                    })
                 }
+
             }
         })
-        for( const [name,arr] of Object.entries(ItemPosts)){
-            console.log(name + " contain "+ arr.length + " entries.")
-            hexo.locals.set(name, function(){
-                return arr
-              });
-            //console.log(arr)
-            const Query = this.model('Post').Query;
-            for(const[key,value] of Object.entries(arr)){
-                if (!value.length) continue;
-                generate(slugize(key), new Query(value), {key: key});
-            }
+    })
+    const Query = this.model('Post').Query;
+    for (const [name, arr] of Object.entries(ItemPosts)) {
+        for (const [key, value] of Object.entries(arr)) {
+            if (!value.length) continue;
+            generate(slugize(key), new Query(value), { key: key });
         }
-        
-    
-    
-  
+    }
+
+    const keys = Object.keys(ItemPosts)
+
+    keys.forEach(key => {
+        if (!Object.prototype.hasOwnProperty.call(last, key)) {
+            last[key] = []
+        }
+        Object.keys(ItemPosts[key]).forEach(subkey => {
+            last[key].push({
+                name: subkey,
+                path: slugize(subkey)
+            })
+        })
+
+    })
+
+
+
+
+
     return result;
+
+
 });
+
+hexo.extend.filter.register('template_locals', function (locals) {
+    Object.keys(last).forEach(key => {
+        locals.site[key] = last[key]
+    })
+    return locals;
+});
+
+
